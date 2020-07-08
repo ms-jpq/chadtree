1e691355851a2b4d3184dfa4086c8ee6d0c9e052
Fly me to the moon <git@bigly.dog>
2020-07-08 13:30:28 -0700

    render

--- rplugin/python3/fm/render.py
+++ rplugin/python3/fm/render.py
@@ -1,23 +1,23 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from enum import Flag, IntEnum
+from enum import Enum, Flag, auto
 from itertools import chain
 from locale import strxfrm
-from typing import Any, Callable, Iterable, Iterator, List, Union, cast
+from typing import Any, Iterable, Iterator, List, Union, cast
 
 from fs import Dir, File, Node
 
 
-class CompVals(IntEnum):
-    FOLDER = 1
-    FILE = 2
+class CompVals(Enum):
+    FOLDER = auto()
+    FILE = auto()
 
 
 class Highlights(Flag):
-    FILE = 0
-    FOLDER = 1
-    LINK = 2
+    FILE = auto()
+    FOLDER = auto()
+    LINK = auto()
 
 
 @dataclass
@@ -25,9 +25,10 @@ class DisplayNode:
     original: Node
     name: str
     children: Iterable[DisplayNode]
+    highlight: Highlights
 
 
-def comp(node: Node) -> Iterable[Union[int, str]]:
+def comp(node: Node) -> Iterable[Union[CompVals, str]]:
     if type(node) == File:
         node = cast(File, node)
         return (CompVals.FILE, strxfrm(node.name), strxfrm(node.ext))
@@ -37,11 +38,32 @@ def comp(node: Node) -> Iterable[Union[int, str]]:
 
 def dparse(node: Node) -> DisplayNode:
     if type(node) == File:
-        return DisplayNode(original=node, name=node.name, children=())
+        highlight = Highlights.FILE
+        if node.is_link:
+            highlight = highlight & Highlights.LINK
+        return DisplayNode(
+            original=node, name=node.name, children=(), highlight=highlight
+        )
     else:
         node = cast(Dir, node)
         descendants: List[Node] = sorted(
             chain(node.files or (), node.children or ()), key=comp
         )
         children = tuple(map(dparse, descendants))
-        return DisplayNode(original=node, name=node.name, children=children)
+        highlight = Highlights.FOLDER
+        if node.is_link:
+            highlight = highlight & Highlights.LINK
+        return DisplayNode(
+            original=node, name=node.name, children=children, highlight=highlight
+        )
+
+
+def render(node: DisplayNode) -> List[str]:
+    def render(node: DisplayNode, *, depth: int) -> Iterator[str]:
+        spaces = depth * 2 * " "
+        rend = node.name
+        yield spaces + rend
+        for child in node.children:
+            yield from render(child, depth=depth + 1)
+
+    return [*render(node, depth=0)]
--- testing.py
+++ testing.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
 from asyncio import run
+from os import getcwd
+
+from rplugin.python3.fm.fs import parse
+from rplugin.python3.fm.render import dparse, render
 
 
 async def main() -> None:
+    cwd = getcwd()
+    n = parse(cwd)
+    d = dparse(n)
+    r = render(d)
+    print(r)
+
     pass
 
 
