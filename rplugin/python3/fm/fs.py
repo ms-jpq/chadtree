8958ea13eaf2988d64d820a8bd47c0f16c501eb2
Fly me to the moon <git@bigly.dog>
2020-07-08 20:08:45 -0700

    added unify

--- rplugin/python3/fm/fs.py
+++ rplugin/python3/fm/fs.py
@@ -1,4 +1,25 @@
+# from shutil import move as fs_move
+from __future__ import annotations
+
+from os.path import dirname, sep
+from typing import Iterator
+
 from .types import Selection
+
+
+def ancestors(path: str) -> Iterator[str]:
+    if not path or path == sep:
+        return
+    else:
+        parent = dirname(path)
+        yield from ancestors(parent)
+        yield parent
+
+
+def unify(paths: Selection) -> Iterator[str]:
+    for path in paths:
+        if not any(a in paths for a in ancestors(path)):
+            yield path
 
 
 def rename(src: str, dest: str) -> None:
--- testing.py
+++ testing.py
@@ -4,15 +4,17 @@ from asyncio import run
 from os import getcwd
 
 from rplugin.python3.fm.cartographer import new
-from rplugin.python3.fm.render import dparse, render
+from rplugin.python3.fm.fs import unify
+from rplugin.python3.fm.render import render
 
 
 async def main() -> None:
     cwd = getcwd()
-    n = new(cwd, index={cwd})
-    r = render(n)
-    print(r, sep="\n")
-
+    # n = new(cwd, index={cwd})
+    # r = render(n)
+    # print(r, sep="\n")
+    a = unify({"/bin", "/bin/bash", ".venv"})
+    print(*a, sep="\n")
     pass
 
 
