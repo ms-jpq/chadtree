99c32552408a2b63b72ebab165c931e6666467e5
Fly me to the moon <git@bigly.dog>
2020-07-08 20:48:05 -0700

    move

--- rplugin/python3/fm/fs.py
+++ rplugin/python3/fm/fs.py
@@ -1,7 +1,10 @@
-# from shutil import move as fs_move
 from __future__ import annotations
 
-from os.path import dirname, sep
+from os import remove as rm
+from os.path import basename, dirname, exists, isdir, join, sep
+from shutil import copy2, copytree
+from shutil import move as mv
+from shutil import rmtree
 from typing import Iterator
 
 from .types import Selection
@@ -23,16 +26,34 @@ def unify(paths: Selection) -> Iterator[str]:
 
 
 def rename(src: str, dest: str) -> None:
-    pass
+    if exists(dest):
+        raise OSError("destination exists")
+    else:
+        mv(src, dest)
 
 
-def remove(src: Selection, dest: str) -> None:
-    pass
+def remove(src: Selection) -> None:
+    for path in unify(src):
+        if isdir(path):
+            rmtree(path)
+        else:
+            rm(path)
 
 
 def move(src: Selection, dest: str) -> None:
-    pass
+    dst_dir = dest if isdir(dest) else dirname(dest)
+    for path in unify(src):
+        name = basename(path)
+        dst = join(dst_dir, name)
+        mv(path, dst)
 
 
 def copy(src: Selection, dest: str) -> None:
-    pass
+    dst_dir = dest if isdir(dest) else dirname(dest)
+    for path in unify(src):
+        name = basename(path)
+        dst = join(dst_dir, name)
+        if isdir(path):
+            copytree(path, dst)
+        else:
+            copy2(path, dst)
