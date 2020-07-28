63c68392c0754fc032b9f04378a70b1e7f49c039
Fly me to the moon <git@bigly.dog>
2020-07-28 09:22:25 -0700

    qf ancestors

--- rplugin/python3/fancy_fm/quickfix.py
+++ rplugin/python3/fancy_fm/quickfix.py
@@ -1,8 +1,10 @@
 from collections import Counter, defaultdict
+from itertools import chain
 from typing import Iterator, Sequence
 
 from pynvim import Nvim
 
+from .fs import ancestors
 from .nvim import call
 from .types import QuickFix
 
@@ -20,7 +22,8 @@ async def quickfix(nvim: Nvim) -> QuickFix:
         return tuple(c())
 
     filenames = await call(nvim, cont)
-    count = Counter(filenames)
+    parents = (ancestor for filename in filenames for ancestor in ancestors(filename))
+    count = Counter(chain(filenames, parents))
     locations = defaultdict(int, count)
     qf = QuickFix(locations=locations)
     return qf
