eb6dca0e42f84e695ee91e02b70301044bba12fb
Fly me to the moon <git@bigly.dog>
2020-08-04 17:36:31 -0700

    added prepare

---
+++ ci/prepare.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from os import environ, getcwd
+from os.path import isdir
+from subprocess import run
+
+
+def call(prog: str, *args: str, cwd: str = getcwd()) -> None:
+    ret = run((prog, *args), cwd=cwd)
+    if ret.returncode != 0:
+        exit(ret.returncode)
+
+
+def git_clone(name: str) -> None:
+    if not isdir(name):
+        token = environ["CI_TOKEN"]
+        uri = f"https://ms-jpq:{token}@github.com/ms-jpq/chadtree.git"
+        email = "ci@ci.ci"
+        username = "ci-bot"
+        call("git", "clone", uri, name)
+        call("git", "config", "user.email", email, cwd=name)
+        call("git", "config", "user.name", username, cwd=name)
+
+
+def build() -> None:
+    call("./temp/ci/build.py")
+
+
+def main() -> None:
+    git_clone("temp")
+    build()
