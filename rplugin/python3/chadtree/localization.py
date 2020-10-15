a5373d794a28745771c3cd742a656493bc05d8a7
Fly me to the moon <git@bigly.dog>
2020-10-15 05:15:58 -0700

    ok

--- rplugin/python3/chadtree/localization.py
+++ rplugin/python3/chadtree/localization.py
@@ -5,30 +5,28 @@ from typing import Dict, Optional, Protocol, cast
 
 from .da import load_json
 
-ISO_LANG = "C"
-
-
 spec: Dict[str, str] = {}
-dspec: Dict[str, str] = {}
+fspec: Dict[str, str] = {}
 
 
-def get_lang(code: Optional[str]) -> str:
+def get_lang(code: Optional[str], fallback: str) -> str:
     if code:
         return code
     else:
         tag, _ = getdefaultlocale()
-        tag = tag or ISO_LANG
+        tag = tag or fallback
         primary, _, _ = tag.partition("-")
         lang = primary.upper()
         return lang
 
 
-def init(root: str, code: Optional[str]) -> None:
-    global spec, dspec
-    lang = get_lang(code)
-    ls, lf = join(root, lang), join(root, ISO_LANG)
+def init(root: str, code: Optional[str], fallback: str) -> None:
+    global spec, fspec
+    lang = get_lang(code, fallback=fallback)
+    ls, lf = join(root, lang), join(root, fallback)
+
     spec = cast(Dict[str, str], load_json(ls))
-    dspec = cast(Dict[str, str], load_json(lf))
+    fspec = cast(Dict[str, str], load_json(lf))
 
 
 class Localizer(Protocol):
@@ -38,7 +36,7 @@ class Localizer(Protocol):
 
 def localize() -> Localizer:
     def locale(key: str, **kwargs: str) -> str:
-        template = spec.get(key, dspec[key])
+        template = spec.get(key, fspec[key])
         return Template(template).substitute(**kwargs)
 
     return locale
