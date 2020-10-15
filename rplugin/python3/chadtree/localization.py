e3bcbc27a7134fd1dd85c440860230e11d220705
Fly me to the moon <git@bigly.dog>
2020-10-15 05:09:23 -0700

    C vs EN lang code

--- rplugin/python3/chadtree/localization.py
+++ rplugin/python3/chadtree/localization.py
@@ -1,7 +1,7 @@
 from locale import getdefaultlocale
 from os.path import join
 from string import Template
-from typing import Dict, Protocol, cast
+from typing import Dict, Optional, Protocol, cast
 
 from .da import load_json
 
@@ -12,17 +12,20 @@ spec: Dict[str, str] = {}
 dspec: Dict[str, str] = {}
 
 
-def get_lang() -> str:
-    tag, _ = getdefaultlocale()
-    tag = tag or ISO_LANG
-    primary, _, _ = tag.partition("-")
-    lang = primary.upper()
-    return lang
+def get_lang(code: Optional[str]) -> str:
+    if code:
+        return code
+    else:
+        tag, _ = getdefaultlocale()
+        tag = tag or ISO_LANG
+        primary, _, _ = tag.partition("-")
+        lang = primary.upper()
+        return lang
 
 
-def init(root: str) -> None:
+def init(root: str, code: Optional[str]) -> None:
     global spec, dspec
-    lang = get_lang()
+    lang = get_lang(code)
     ls, lf = join(root, lang), join(root, ISO_LANG)
     spec = cast(Dict[str, str], load_json(ls))
     dspec = cast(Dict[str, str], load_json(lf))
