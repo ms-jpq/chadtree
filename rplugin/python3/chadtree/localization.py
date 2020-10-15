fa5a3c27942c7c0646f91f7a728545dffd931f91
Fly me to the moon <git@bigly.dog>
2020-10-15 04:51:58 -0700

    locale

--- rplugin/python3/chadtree/localization.py
+++ rplugin/python3/chadtree/localization.py
@@ -1,1 +1,30 @@
+from locale import getdefaultlocale
+from string import Template
+from typing import Dict, Protocol
 
+ISO_LANG = "C"
+
+
+def get_lang() -> str:
+    tag, _ = getdefaultlocale()
+    tag = tag or ISO_LANG
+    primary, _, _ = tag.partition("-")
+    lang = primary.upper()
+    return lang
+
+
+class Localizer(Protocol):
+    def __call__(self, key: str, **kwargs: str) -> str:
+        return ""
+
+
+def localize(root: str) -> Localizer:
+    lang = get_lang()
+    specs: Dict[str, Dict[str, str]] = {}
+    spec, dspec = specs[lang], specs[ISO_LANG]
+
+    def locale(key: str, **kwargs: str) -> str:
+        template = spec.get(key, dspec[key])
+        return Template(template).substitute(**kwargs)
+
+    return locale
