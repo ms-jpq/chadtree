fff3925ca77c9274783943997f0cf92976370e2e
Fly me to the moon <git@bigly.dog>
2020-10-15 05:06:48 -0700

    better locale

--- rplugin/python3/chadtree/localization.py
+++ rplugin/python3/chadtree/localization.py
@@ -1,8 +1,15 @@
 from locale import getdefaultlocale
+from os.path import join
 from string import Template
-from typing import Dict, Protocol
+from typing import Dict, Protocol, cast
+
+from .da import load_json
 
 ISO_LANG = "C"
+
+
+spec: Dict[str, str] = {}
+dspec: Dict[str, str] = {}
 
 
 def get_lang() -> str:
@@ -13,16 +20,20 @@ def get_lang() -> str:
     return lang
 
 
+def init(root: str) -> None:
+    global spec, dspec
+    lang = get_lang()
+    ls, lf = join(root, lang), join(root, ISO_LANG)
+    spec = cast(Dict[str, str], load_json(ls))
+    dspec = cast(Dict[str, str], load_json(lf))
+
+
 class Localizer(Protocol):
     def __call__(self, key: str, **kwargs: str) -> str:
         return ""
 
 
-def localize(root: str) -> Localizer:
-    lang = get_lang()
-    specs: Dict[str, Dict[str, str]] = {}
-    spec, dspec = specs[lang], specs[ISO_LANG]
-
+def localize() -> Localizer:
     def locale(key: str, **kwargs: str) -> str:
         template = spec.get(key, dspec[key])
         return Template(template).substitute(**kwargs)
