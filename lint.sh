c395dbfc687dfcc30f490105aee18587aaaffd66
Fly me to the moon <git@bigly.dog>
2020-12-10 21:52:35 -0800

    better linter

--- lint.sh
+++ lint.sh
@@ -6,9 +6,25 @@ set -o pipefail
 
 cd "$(dirname "$0")" || exit 1
 
-FILES=(
-  ci/*.py
-  rplugin/python3/chadtree/*.py
-  )
 
-mypy -- "${FILES[@]}"
+readarray -t -d $'\0' TRACKED < <(git ls-files --exclude-standard -z)
+readarray -t -d $'\0' UNTRACKED < <(git ls-files --exclude-standard --others -z)
+
+ALL_FILES=("${TRACKED[@]}" "${UNTRACKED[@]}")
+PYTHON_FILES=()
+for FILE in "${ALL_FILES[@]}";
+do
+  case "$FILE" in
+    *.py)
+      if [[ -f $FILE ]]
+      then
+        PYTHON_FILES+=("$PWD/$FILE")
+      fi
+      ;;
+    *)
+      ;;
+  esac
+done
+
+
+mypy -- "${PYTHON_FILES[@]}"
