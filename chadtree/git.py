e99b3cf04d8856a5ff5bb49e174c4edca84097e0
Fly me to the moon <git@bigly.dog>
2021-01-03 22:27:18 +0700

    ok

--- chadtree/git.py
+++ chadtree/git.py
@@ -3,22 +3,23 @@ from locale import strxfrm
 from os import linesep
 from os.path import join, sep
 from shutil import which
-from typing import Mapping, Iterator, Set, Tuple
+from typing import Iterator, Mapping, Set, Tuple
 
 from std2.asyncio.subprocess import call
+
 from .fs import ancestors
 from .types import VCStatus
 
-GIT_LIST_CMD = ("git", "status", "--ignored", "--renames", "--porcelain")
-GIT_SUBMODULE_MARKER = "Entering "
-GIT_ENV = {"LC_ALL": "C"}
+_GIT_LIST_CMD = ("git", "status", "--ignored", "--renames", "--porcelain")
+_GIT_SUBMODULE_MARKER = "Entering "
+_GIT_ENV = {"LC_ALL": "C"}
 
 
 class GitError(Exception):
     pass
 
 
-async def root() -> str:
+async def _root() -> str:
     ret = await call("git", "rev-parse", "--show-toplevel")
     if ret.code != 0:
         raise GitError(ret.err)
@@ -26,8 +27,8 @@ async def root() -> str:
         return ret.out.rstrip()
 
 
-async def stat_main() -> Mapping[str, str]:
-    ret = await call(*GIT_LIST_CMD, "-z")
+async def _stat_main() -> Mapping[str, str]:
+    ret = await call(*_GIT_LIST_CMD, "-z")
     if ret.code != 0:
         raise GitError(ret.err)
     else:
@@ -44,14 +45,14 @@ async def stat_main() -> Mapping[str, str]:
         return entries
 
 
-async def stat_sub_modules() -> Mapping[str, str]:
+async def _stat_sub_modules() -> Mapping[str, str]:
     ret = await call(
         "git",
         "submodule",
         "foreach",
         "--recursive",
-        " ".join(GIT_LIST_CMD),
-        env=GIT_ENV,
+        " ".join(_GIT_LIST_CMD),
+        env=_GIT_ENV,
     )
     if ret.code != 0:
         raise GitError(ret.err)
@@ -61,8 +62,8 @@ async def stat_sub_modules() -> Mapping[str, str]:
         def cont() -> Iterator[Tuple[str, str]]:
             root = ""
             for line in it:
-                if line.startswith(GIT_SUBMODULE_MARKER):
-                    root = line[len(GIT_SUBMODULE_MARKER) + 1 : -1].rstrip(linesep)
+                if line.startswith(_GIT_SUBMODULE_MARKER):
+                    root = line[len(_GIT_SUBMODULE_MARKER) + 1 : -1].rstrip(linesep)
                 else:
                     prefix, file = line[:2], line[3:]
                     yield prefix, join(root, file.rstrip(sep))
@@ -73,7 +74,7 @@ async def stat_sub_modules() -> Mapping[str, str]:
         return entries
 
 
-def parse(root: str, stats: Mapping[str, str]) -> VCStatus:
+def _parse(root: str, stats: Mapping[str, str]) -> VCStatus:
     ignored: Set[str] = set()
     status: Mapping[str, str] = {}
     directories: Mapping[str, Set[str]] = {}
@@ -98,9 +99,9 @@ def parse(root: str, stats: Mapping[str, str]) -> VCStatus:
 async def status() -> VCStatus:
     if which("git"):
         try:
-            r, s_main, s_sub = await gather(root(), stat_main(), stat_sub_modules())
+            r, s_main, s_sub = await gather(_root(), _stat_main(), _stat_sub_modules())
             stats = {**s_sub, **s_main}
-            return parse(r, stats)
+            return _parse(r, stats)
         except GitError:
             return VCStatus()
     else:
