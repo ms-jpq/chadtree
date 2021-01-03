808793159f8c5c6c53731f00dde578f8d6c82d4f
Fly me to the moon <git@bigly.dog>
2021-01-03 22:23:19 +0700

    del code

--- chadtree/nvim.py
+++ chadtree/nvim.py
@@ -1,12 +1,5 @@
-from typing import Any, Sequence, Tuple, TypeVar
-
 from pynvim import Nvim
-from pynvim.api.common import NvimError
 from pynvim_pp.lib import async_call
-
-T = TypeVar("T")
-
-
 
 
 async def getcwd(nvim: Nvim) -> str:
