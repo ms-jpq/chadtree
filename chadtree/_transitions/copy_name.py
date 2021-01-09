0699b94ea481d24f905faf8ed17237ae21023b09
Fly me to the moon <git@bigly.dog>
2021-01-10 00:20:16 +0700

    more

--- chadtree/_transitions/copy_name.py
+++ chadtree/_transitions/copy_name.py
@@ -1,7 +1,19 @@
+from locale import strxfrm
+from os import linesep
+from typing import Iterator
+
+from pynvim import Nvim
+from pynvim_pp.lib import s_write
+
+from ..registry import rpc
+from ..settings.localization import LANG
+from ..settings.types import Settings
+from ..state.ops import indices
+from ..state.types import State
 
 
-@rpc(blocking=False, name="CHADcopy_name")
-def c_copy_name(nvim: Nvim, state: State, settings: Settings, is_visual: bool) -> None:
+@rpc(blocking=False)
+def _copy_name(nvim: Nvim, state: State, settings: Settings, is_visual: bool) -> None:
     """
     Copy dirname / filename
     """
@@ -9,7 +21,7 @@ def c_copy_name(nvim: Nvim, state: State, settings: Settings, is_visual: bool) -
     def gen_paths() -> Iterator[str]:
         selection = state.selection
         if is_visual or not selection:
-            nodes = _indices(nvim, state=state, is_visual=is_visual)
+            nodes = indices(nvim, state=state, is_visual=is_visual)
             for node in nodes:
                 yield node.path
         else:
@@ -24,5 +36,3 @@ def c_copy_name(nvim: Nvim, state: State, settings: Settings, is_visual: bool) -
     nvim.funcs.setreg("+", clip)
     nvim.funcs.setreg("*", clip)
     s_write(nvim, LANG("copy_paths", copied_paths=copied_paths))
-
-
--- chadtree/_transitions/toggles.py
+++ chadtree/_transitions/toggles.py
@@ -1,3 +1,15 @@
+from pynvim import Nvim
+from pynvim_pp.lib import s_write
+
+from ..registry import rpc
+from ..settings.localization import LANG
+from ..settings.types import Settings
+from ..state.next import forward
+from ..state.types import State
+from ..version_ctl.git import status
+from ..version_ctl.types import VCStatus
+from .types import Stage
+
 
 @rpc(blocking=False, name="CHADtoggle_hidden")
 def c_hidden(nvim: Nvim, state: State, settings: Settings, is_visual: bool) -> Stage:
@@ -29,7 +41,7 @@ def c_toggle_vc(nvim: Nvim, state: State, settings: Settings, is_visual: bool) -
     """
 
     enable_vc = not state.enable_vc
-    vc = _vc_stat(enable_vc)
+    vc = status() if enable_vc else VCStatus()
     new_state = forward(state, settings=settings, enable_vc=enable_vc, vc=vc)
     s_write(nvim, LANG("version_control_indi", enable_vc=str(new_state.enable_vc)))
     return Stage(new_state)
