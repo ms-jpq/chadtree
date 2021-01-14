1ab6efb2a55801b1c6aad74c3c5beb954dac41c0
Fly me to the moon <git@bigly.dog>
2021-01-14 19:12:10 +0700

    quit works

--- chadtree/transitions/quit.py
+++ chadtree/transitions/quit.py
@@ -7,19 +7,15 @@ from ..state.types import State
 from .shared.wm import find_fm_windows_in_tab
 
 
-def _kill_fm_windows(nvim: Nvim) -> None:
-    wins = list_wins(nvim)
-    if len(wins) <= 1:
-        nvim.api.command("quit")
-    else:
-        for win in find_fm_windows_in_tab(nvim):
-            win_close(nvim, win=win)
-
-
 @rpc(blocking=False)
 def _quit(nvim: Nvim, state: State, settings: Settings, is_visual: bool) -> None:
     """
     Close sidebar
     """
 
-    _kill_fm_windows(nvim)
+    wins = list_wins(nvim)
+    if len(wins) <= 1:
+        nvim.api.command("quit")
+    else:
+        for win in find_fm_windows_in_tab(nvim):
+            win_close(nvim, win=win)
