5dc00993f80db97c45d5e92226f729a44ca483f2
Fly me to the moon <git@bigly.dog>
2021-01-14 17:26:52 +0700

    iso

--- chadtree/transitions/selection.py
+++ chadtree/transitions/selection.py
@@ -23,23 +23,12 @@ def _clear_selection(
 
 
 @rpc(blocking=False)
-def _select(
-    nvim: Nvim, state: State, settings: Settings, is_visual: bool
-) -> Optional[Stage]:
+def _select(nvim: Nvim, state: State, settings: Settings, is_visual: bool) -> Stage:
     """
     Folder / File -> select
     """
 
     nodes = indices(nvim, state=state, is_visual=is_visual)
-    if is_visual:
-        selection = state.selection ^ {node.path for node in nodes}
-        new_state = forward(state, settings=settings, selection=selection)
-        return Stage(new_state)
-    else:
-        node = next(nodes, None)
-        if node:
-            selection = state.selection ^ {node.path}
-            new_state = forward(state, settings=settings, selection=selection)
-            return Stage(new_state)
-        else:
-            return None
+    selection = state.selection ^ {node.path for node in nodes}
+    new_state = forward(state, settings=settings, selection=selection)
+    return Stage(new_state)
