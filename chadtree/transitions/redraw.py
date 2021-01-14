54c2dee19c78f66ad714f372071978b2646b1dc4
Fly me to the moon <git@bigly.dog>
2021-01-14 19:10:32 +0700

    buf update preserve cursor pos

--- chadtree/transitions/redraw.py
+++ chadtree/transitions/redraw.py
@@ -3,6 +3,7 @@ from typing import Optional, Sequence, cast
 from pynvim import Nvim
 from pynvim_pp.api import cur_win, win_get_cursor
 from pynvim_pp.atomic import Atomic
+from pynvim_pp.operators import operator_marks
 
 from ..consts import FM_NAMESPACE
 from ..state.types import State
@@ -23,8 +24,9 @@ def redraw(nvim: Nvim, state: State, focus: Optional[str]) -> None:
     cwin = cur_win(nvim)
     ns = nvim.api.create_namespace(FM_NAMESPACE)
 
-    for win, buffer in find_fm_windows(nvim):
+    for win, buf in find_fm_windows(nvim):
         row, col = win_get_cursor(nvim, win=win)
+        (r1, c1), (r2, c2) = operator_marks(nvim, buf=buf, visual_type=None)
         new_row = (
             focus_row + 1
             if focus_row is not None
@@ -36,20 +38,22 @@ def redraw(nvim: Nvim, state: State, focus: Optional[str]) -> None:
         )
 
         atomic = Atomic()
-        atomic.buf_clear_namespace(buffer, ns, 0, -1)
-        atomic.buf_set_option(buffer, "modifiable", True)
-        atomic.buf_set_lines(buffer, 0, -1, True, lines)
-        atomic.buf_set_option(buffer, "modifiable", False)
+        atomic.buf_clear_namespace(buf, ns, 0, -1)
+        atomic.buf_set_option(buf, "modifiable", True)
+        atomic.buf_set_lines(buf, 0, -1, True, lines)
+        atomic.buf_set_option(buf, "modifiable", False)
 
         vtext = cast(Sequence[Sequence[Badge]], badges)
         for idx, bgs in enumerate(vtext):
             vtxt = tuple((badge.text, badge.group) for badge in bgs)
-            atomic.buf_set_virtual_text(buffer, ns, idx, vtxt, {})
+            atomic.buf_set_virtual_text(buf, ns, idx, vtxt, {})
 
         hl2 = cast(Sequence[Sequence[Highlight]], highlights)
         for idx, hl in enumerate(hl2):
             for h in hl:
-                atomic.buf_add_highlight(buffer, ns, h.group, idx, h.begin, h.end)
+                atomic.buf_add_highlight(buf, ns, h.group, idx, h.begin, h.end)
 
         atomic.win_set_cursor(win, (new_row, col))
+        atomic.call_function("setpos", ("'<", (buf.number, r1 + 1, c1 + 1, 0)))
+        atomic.call_function("setpos", ("'>", (buf.number, r2 + 1, c2 + 1, 0)))
         atomic.commit(nvim)
