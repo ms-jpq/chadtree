2eb86740e533cd1188f084371609c43b322c68f3
Fly me to the moon <git@bigly.dog>
2021-01-16 00:59:02 +0700

    make open sys async

--- chadtree/transitions/open_system.py
+++ chadtree/transitions/open_system.py
@@ -2,9 +2,10 @@ from shutil import which
 from subprocess import DEVNULL, PIPE, CalledProcessError, check_call
 
 from pynvim import Nvim
-from pynvim_pp.lib import write
+from pynvim_pp.lib import awrite
+from pynvim_pp.logging import log
 
-from ..registry import rpc
+from ..registry import pool, rpc
 from ..settings.localization import LANG
 from ..settings.types import Settings
 from ..state.types import State
@@ -32,7 +33,13 @@ def _open_sys(nvim: Nvim, state: State, settings: Settings, is_visual: bool) -> 
     if not node:
         return None
     else:
-        try:
-            _open_gui(node.path)
-        except (CalledProcessError, LookupError) as e:
-            write(nvim, e)
+
+        def cont() -> None:
+            try:
+                _open_gui(node.path)
+            except (CalledProcessError, LookupError) as e:
+                awrite(nvim, e)
+            except Exception as e:
+                log.exception("%s", e)
+
+        pool.submit(cont)
