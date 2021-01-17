11f8ee7b2fde006451d65f8111fc273299988b9a
Fly me to the moon <git@bigly.dog>
2021-01-18 02:21:01 +0700

    nowipe

--- chadtree/transitions/shared/wm.py
+++ chadtree/transitions/shared/wm.py
@@ -101,7 +101,7 @@ def find_current_buffer_name(nvim: Nvim) -> str:
 
 
 def new_fm_buffer(nvim: Nvim, settings: Settings) -> Buffer:
-    buf = create_buf(nvim, listed=False, scratch=True, wipe=True, nofile=True)
+    buf = create_buf(nvim, listed=False, scratch=True, wipe=False, nofile=True)
     buf_set_option(nvim, buf=buf, key="modifiable", val=False)
     buf_set_option(nvim, buf=buf, key="filetype", val=FM_FILETYPE)
 
