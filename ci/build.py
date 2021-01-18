fc462a04a4d8622b947ba6aaf1dd48ade0e88bc4
Fly me to the moon <git@bigly.dog>
2021-01-18 08:56:20 +0700

    okie

--- ci/build.py
+++ ci/build.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from subprocess import check_call, check_output, run
 from typing import Any, Iterator, Mapping, Optional, Sequence
 
+from std2.coloursys import hex_inverse
 from std2.pickle import decode, encode
 from std2.tree import merge, recur_sort
 from std2.urllib import urlopen
@@ -31,53 +32,53 @@ SRC_COLOUR = "colours"
 
 
 @dataclass(frozen=True)
-class GithubColours:
+class _GithubColours:
     extensions: Sequence[str] = ()
     color: Optional[str] = None
 
 
-GithubSpec = Mapping[str, GithubColours]
+GithubSpec = Mapping[str, _GithubColours]
 
 
 @dataclass(frozen=True)
-class IconFolderFormat:
+class _IconFolderFormat:
     open: str
     closed: str
 
 
 @dataclass(frozen=True)
-class IconLoadFormat:
+class _IconLoadFormat:
     extensions: Mapping[str, str]
     exact: Mapping[str, str]
     glob: Mapping[str, str]
     default: str
-    folder: IconFolderFormat
+    folder: _IconFolderFormat
 
 
 @dataclass(frozen=True)
-class IconDumpFormat:
+class _IconDumpFormat:
     type: Mapping[str, str]
     name_exact: Mapping[str, str]
     name_glob: Mapping[str, str]
     default_icon: str
-    folder: IconFolderFormat
+    folder: _IconFolderFormat
 
 
 @dataclass(frozen=True)
-class ColoursLoadFormat:
+class _ColoursLoadFormat:
     extensions: Mapping[str, str]
     exact: Mapping[str, str]
     glob: Mapping[str, str]
 
 
 @dataclass(frozen=True)
-class ColoursDumpFormat:
+class _ColoursDumpFormat:
     type: Mapping[str, str]
     name_exact: Mapping[str, str]
     name_glob: Mapping[str, str]
 
 
-def fetch(uri: str) -> str:
+def _fetch(uri: str) -> str:
     with urlopen(uri) as resp:
         code = resp.getcode()
         body = resp.read()
@@ -87,48 +88,61 @@ def fetch(uri: str) -> str:
         return body.decode()
 
 
-def spit_json(path: Path, json: Any) -> None:
+def _spit_json(path: Path, json: Any) -> None:
     path.parent.mkdir(exist_ok=True, parents=True)
     sorted_json = recur_sort(json)
     with path.open("w") as fd:
         dump(sorted_json, fd, ensure_ascii=False, check_circular=False, indent=2)
 
 
-def process_exts(exts: Mapping[str, str]) -> Mapping[str, str]:
+def _process_exts(exts: Mapping[str, str]) -> Mapping[str, str]:
     return {f".{k}": v for k, v in exts.items()}
 
 
-def process_glob(glob: Mapping[str, str]) -> Mapping[str, str]:
+def _process_glob(glob: Mapping[str, str]) -> Mapping[str, str]:
     return {k.rstrip("$").replace(r"\.", "."): v for k, v in glob.items()}
 
 
-def process_icons(json: Any) -> IconDumpFormat:
-    loaded: IconLoadFormat = decode(IconLoadFormat, json)
-    dump = IconDumpFormat(
-        type=process_exts(loaded.extensions),
+def _process_icons(json: Any) -> _IconDumpFormat:
+    loaded: _IconLoadFormat = decode(_IconLoadFormat, json)
+    dump = _IconDumpFormat(
+        type=_process_exts(loaded.extensions),
         name_exact=loaded.exact,
-        name_glob=process_glob(loaded.glob),
+        name_glob=_process_glob(loaded.glob),
         default_icon=loaded.default,
         folder=loaded.folder,
     )
     return dump
 
 
-def process_hexcode(colours: Mapping[str, str]) -> Mapping[str, str]:
+def _process_hexcode(colours: Mapping[str, str]) -> Mapping[str, str]:
     return {k: f"#{v}" for k, v in colours.items()}
 
 
-def process_colours(json: Any) -> ColoursDumpFormat:
-    loaded: ColoursLoadFormat = decode(ColoursLoadFormat, json)
-    dump = ColoursDumpFormat(
-        type=process_hexcode(process_exts(loaded.extensions)),
-        name_exact=process_hexcode(loaded.exact),
-        name_glob=process_hexcode(process_glob(loaded.glob)),
+def _process_colours(json: Any) -> _ColoursDumpFormat:
+    loaded: _ColoursLoadFormat = decode(_ColoursLoadFormat, json)
+    dump = _ColoursDumpFormat(
+        type=_process_hexcode(_process_exts(loaded.extensions)),
+        name_exact=_process_hexcode(loaded.exact),
+        name_glob=_process_hexcode(_process_glob(loaded.glob)),
     )
     return dump
 
 
-def devicons() -> None:
+def _trans_inverse(mapping: Mapping[str, str]) -> Mapping[str, str]:
+    return {key: hex_inverse(val) for key, val in mapping.items()}
+
+
+def _invert_nightmode(night_mode: _ColoursDumpFormat) -> _ColoursDumpFormat:
+    dump = _ColoursDumpFormat(
+        type=_trans_inverse(night_mode.type),
+        name_exact=_trans_inverse(night_mode.name_exact),
+        name_glob=_trans_inverse(night_mode.name_glob),
+    )
+    return dump
+
+
+def _devicons() -> None:
     TEMP.mkdir(parents=True, exist_ok=True)
 
     image = "chad-icons"
@@ -144,28 +158,32 @@ def devicons() -> None:
         src = f"{container}:/root/{icon}.json"
         check_call(("docker", "cp", src, str(TEMP_JSON)))
 
-        parsed = process_icons(load(TEMP_JSON.open()))
+        parsed = _process_icons(load(TEMP_JSON.open()))
         basic = safe_load((ASSETS / icon).with_suffix(".base.yml").read_bytes())
         merged = merge(encode(parsed), basic)
 
         dest = (ARTIFACTS / icon).with_suffix(".json")
-        spit_json(dest, merged)
+        _spit_json(dest, merged)
 
     ascii_json = "ascii_icons"
     json = safe_load((ASSETS / ascii_json).with_suffix(".base.yml").read_bytes())
-    spit_json((ARTIFACTS / ascii_json).with_suffix(".json"), json)
+    _spit_json((ARTIFACTS / ascii_json).with_suffix(".json"), json)
 
     src = f"{container}:/root/{SRC_COLOUR}.json"
-    dest = (ARTIFACTS / SRC_COLOUR).with_suffix(".json")
     check_call(("docker", "cp", src, str(TEMP_JSON)))
-    parsed2 = process_colours(load(TEMP_JSON.open()))
-    spit_json(dest, encode(parsed2))
+    night_mode = _process_colours(load(TEMP_JSON.open()))
+    day_mode = _invert_nightmode(night_mode)
 
+    night_dest = (ARTIFACTS / f"{SRC_ICONS}_night").with_suffix(".json")
+    day_dest = (ARTIFACTS / f"{SRC_ICONS}_day").with_suffix(".json")
+
+    _spit_json(night_dest, encode(night_mode))
+    _spit_json(day_dest, encode(day_mode))
     check_call(("docker", "rm", container))
 
 
-def github_colours() -> None:
-    raw = fetch(LANG_COLOURS)
+def _github_colours() -> None:
+    raw = _fetch(LANG_COLOURS)
     yaml: GithubSpec = decode(GithubSpec, safe_load(raw), strict=False)
     lookup: Mapping[str, str] = {
         ext: spec.color
@@ -174,10 +192,10 @@ def github_colours() -> None:
         if spec.color
     }
 
-    spit_json(LANG_COLOURS_JSON, lookup)
+    _spit_json(LANG_COLOURS_JSON, lookup)
 
 
-def git_alert() -> None:
+def _git_alert() -> None:
     prefix = "update-icons"
     remote_brs = check_output(("git", "branch", "--remotes"), text=True)
 
@@ -209,9 +227,9 @@ def git_alert() -> None:
 
 
 def main() -> None:
-    devicons()
-    github_colours()
-    # git_alert()
+    _devicons()
+    _github_colours()
+    _git_alert()
 
 
 main()
