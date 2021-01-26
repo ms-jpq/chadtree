5724e3f5b38e4a0cafbd53c66d737fa2ab86cdcc
Fly me to the moon <git@bigly.dog>
2021-01-26 00:04:55 -0800

    deps

--- chadtree/__main__.py
+++ chadtree/__main__.py
@@ -30,10 +30,10 @@ def parse_args() -> Namespace:
 
     s_run = sub_parsers.add_parser("run")
     s_run.add_argument("--socket", required=True)
+    s_run.add_argument("--xdg", action="store_true")
 
     s_deps = sub_parsers.add_parser("deps")
-
-    parser.add_argument("--xdg", action="store_true")
+    s_deps.add_argument("--xdg", action="store_true")
 
     return parser.parse_args()
 
