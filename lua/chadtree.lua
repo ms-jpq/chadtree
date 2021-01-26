c7ee012a44de2b603bf47998d0597085a939b4ca
Fly me to the moon <git@bigly.dog>
2021-01-26 13:10:33 -0800

    try lib uv

--- lua/chadtree.lua
+++ lua/chadtree.lua
@@ -1,6 +1,121 @@
 return function(args)
   local cwd = unpack(args)
 
+  local uv = vim.loop
+  local spawn = function(prog, args, input, cwd, env, handlers)
+    local _env = vim.api.nvim_call_function("environ", {})
+    local stdin = uv.new_pipe(false)
+    local stdout = uv.new_pipe(false)
+    local stderr = uv.new_pipe(false)
+    local opts = {
+      stdio = {stdin, stdout, stderr},
+      args = args,
+      cwd = cwd
+    }
+
+    local process, pid = nil, nil
+    process, pid =
+      uv.spawn(
+      prog,
+      opts,
+      function(code)
+        local handles = {stdin, stdout, stderr, process}
+        for _, handle in ipairs(handles) do
+          uv.close(handle)
+        end
+        (handlers.on_exit or function()
+          end)(code)
+      end
+    )
+    assert(process, pid)
+
+    uv.read_start(
+      stdout,
+      function(err, data)
+        assert(not err, err)
+        if data then
+          (handlers.on_stdout or function()
+            end)(code)
+        end
+      end
+    )
+
+    uv.read_start(
+      stderr,
+      function(err, data)
+        assert(not err, err)
+        if data then
+          (handlers.on_stderr or function()
+            end)(code)
+        end
+      end
+    )
+
+    if input then
+      uv.write(
+        stdin,
+        input,
+        function(err)
+          assert(not err, err)
+          uv.shutdown(
+            stdin,
+            function(err)
+              assert(not err, err)
+            end
+          )
+        end
+      )
+    end
+
+    return pid
+  end
+
+  local on_exit = function(code)
+    vim.schedule(
+      function()
+        vim.api.nvim_err_writeln(" | EXITED - " .. code)
+      end
+    )
+  end
+
+  local on_stdout = function(data)
+    vim.schedule(
+      function()
+        vim.api.nvim_out_write(data)
+      end
+    )
+  end
+
+  local on_stderr = function(data)
+    vim.schedule(
+      function()
+        vim.api.nvim_err_write(data)
+      end
+    )
+  end
+
+  local handlers = {on_exit = on_exit, on_stdout = on_stdout, on_stderr = on_stderr}
+
+  local function defer(timeout, callback)
+    local timer = uv.new_timer()
+    uv.timer_start(
+      timer,
+      timeout,
+      0,
+      function()
+        uv.timer_stop(timer)
+        uv.close(timer)
+        vim.schedule(callback)
+      end
+    )
+  end
+
+  --
+  --
+  -- DOMAIN CODE
+  --
+  --
+
   chad = chad or {}
   local linesep = "\n"
   local POLLING_RATE = 10
@@ -13,21 +128,7 @@ return function(args)
     local chad_params = {}
     local err_exit = false
 
-    local function defer(timeout, callback)
-      local timer = vim.loop.new_timer()
-      timer:start(
-        timeout,
-        0,
-        function()
-          timer:stop()
-          timer:close()
-          vim.schedule(callback)
-        end
-      )
-      return timer
-    end
-
-    chad.on_exit = function(args)
+    handlers.on_exit = function(args)
       local code = unpack(args)
       local msg = " | CHADTree EXITED - " .. code
       if not (code == 0 or code == 143) then
@@ -42,16 +143,6 @@ return function(args)
       end
     end
 
-    chad.on_stdout = function(args)
-      local msg = unpack(args)
-      vim.api.nvim_out_write(table.concat(msg, linesep))
-    end
-
-    chad.on_stderr = function(args)
-      local msg = unpack(args)
-      vim.api.nvim_err_write(table.concat(msg, linesep))
-    end
-
     local start = function(...)
       local go, _py3 = pcall(vim.api.nvim_get_var, "python3_host_prog")
       local py3 = go and _py3 or "python3"
@@ -60,21 +151,11 @@ return function(args)
 
       local args =
         vim.tbl_flatten {
-        {py3, "-m", "chadtree"},
+        {"-m", "chadtree"},
         {...},
         (settings.xdg and {"--xdg"} or {})
       }
-      local params = {
-        cwd = cwd,
-        on_exit = "CHADon_exit",
-        on_stdout = "CHADon_stdout",
-        on_stderr = "CHADon_stderr"
-      }
-      if vim.api.nvim_call_function("has", {"win32"}) == 1 then
-        local cmd = table.concat(args, " ")
-        args = vim.api.nvim_call_function("shellescape", {cmd})
-      end
-      local job_id = vim.api.nvim_call_function("jobstart", {args, params})
+      local job_id = spawn(py3, args, cwd, env, handlers)
       return job_id
     end
 
