1c629f24906785c34c2216db96a1c25d079b9164
Fly me to the moon <git@bigly.dog>
2021-01-27 05:26:28 -0800

    back to same old

--- lua/chadtree.lua
+++ lua/chadtree.lua
@@ -1,120 +1,19 @@
 return function(args)
   local cwd = unpack(args)
 
-  local uv = vim.loop
-  local on_exit = function(code)
-    vim.schedule(
-      function()
-        vim.api.nvim_err_writeln(" | EXITED - " .. code)
-      end
-    )
-  end
-  local on_stdout = function(data)
-    vim.schedule(
-      function()
-        vim.api.nvim_out_write(data)
-      end
-    )
-  end
-  local on_stderr = function(data)
-    vim.schedule(
-      function()
-        vim.api.nvim_err_write(data)
-      end
-    )
-  end
-  local spawn = function(prog, args, input, cwd, env, handlers)
-    local _env = {}
-    for key, val in pairs(vim.api.nvim_call_function("environ", {})) do
-      table.insert(_env, key .. "=" .. val)
-    end
-    for key, val in pairs(env) do
-      table.insert(_env, key .. "=" .. val)
-    end
-    local stdin = uv.new_pipe(false)
-    local stdout = uv.new_pipe(false)
-    local stderr = uv.new_pipe(false)
-    local opts = {
-      stdio = {stdin, stdout, stderr},
-      args = args,
-      cwd = cwd,
-      env = _env
-    }
-
-    local process, pid = nil, nil
-    process, pid =
-      uv.spawn(
-      prog,
-      opts,
-      function(code, signal)
-        local handles = {stdin, stdout, stderr, process}
-        for _, handle in ipairs(handles) do
-          uv.close(handle)
-        end
-        (handlers.on_exit or on_exit)(code)
-      end
-    )
-    assert(process, pid)
-
-    uv.read_start(
-      stdout,
-      function(err, data)
-        assert(not err, err)
-        if data then
-          (handlers.on_stdout or on_stdout)(data)
-        end
-      end
-    )
-
-    uv.read_start(
-      stderr,
-      function(err, data)
-        assert(not err, err)
-        if data then
-          (handlers.on_stderr or on_stderr)(data)
-        end
-      end
-    )
-
-    if input then
-      uv.write(
-        stdin,
-        input,
-        function(err)
-          assert(not err, err)
-          uv.shutdown(
-            stdin,
-            function(err)
-              assert(not err, err)
-            end
-          )
-        end
-      )
-    end
-
-    return pid
-  end
-
-
   local function defer(timeout, callback)
-    local timer = uv.new_timer()
-    uv.timer_start(
-      timer,
+    local timer = vim.loop.new_timer()
+    timer:start(
       timeout,
       0,
       function()
-        uv.timer_stop(timer)
-        uv.close(timer)
+        timer:stop()
+        timer:close()
         vim.schedule(callback)
       end
     )
+    return timer
   end
-
-  --
-  --
-  -- DOMAIN CODE
-  --
-  --
 
   chad = chad or {}
   local linesep = "\n"
@@ -128,23 +27,30 @@ return function(args)
     local chad_params = {}
     local err_exit = false
 
-    local handlers = {
-      on_exit = function(code)
-        vim.schedule(function ()
-          local msg = " | CHADTree EXITED - " .. code
-          if not (code == 0 or code == 143) then
-            err_exit = true
-            vim.api.nvim_err_writeln(msg)
-          else
-            err_exit = false
-          end
-          job_id = nil
-          for _, param in ipairs(chad_params) do
-            chad[chad_params] = nil
-          end
-        end)
+    chad.on_exit = function(args)
+      local code = unpack(args)
+      local msg = " | CHADTree EXITED - " .. code
+      if not (code == 0 or code == 143) then
+        err_exit = true
+        vim.api.nvim_err_writeln(msg)
+      else
+        err_exit = false
       end
-    }
+      job_id = nil
+      for _, param in ipairs(chad_params) do
+        chad[chad_params] = nil
+      end
+    end
+
+    chad.on_stdout = function(args)
+      local msg = unpack(args)
+      vim.api.nvim_out_write(table.concat(msg, linesep))
+    end
+
+    chad.on_stderr = function(args)
+      local msg = unpack(args)
+      vim.api.nvim_err_write(table.concat(msg, linesep))
+    end
 
     local start = function(...)
       local go, _py3 = pcall(vim.api.nvim_get_var, "python3_host_prog")
@@ -152,14 +58,20 @@ return function(args)
       local go, _settings = pcall(vim.api.nvim_get_var, "chadtree_settings")
       local settings = go and _settings or {}
 
+      local main = cwd .. "/venv.sh"
+
       local args =
         vim.tbl_flatten {
-        {py3, "-m", "chadtree"},
+        {main, py3, "-m", "chadtree"},
         {...},
         (settings.xdg and {"--xdg"} or {})
       }
-
-      local job_id = spawn("./venv.sh", args, nil, cwd, {}, handlers)
+      local params = {
+        on_exit = "CHADon_exit",
+        on_stdout = "CHADon_stdout",
+        on_stderr = "CHADon_stderr"
+      }
+      local job_id = vim.api.nvim_call_function("jobstart", {args, params})
       return job_id
     end
 
@@ -176,19 +88,10 @@ return function(args)
 
         if not job_id then
           local server = vim.api.nvim_call_function("serverstart", {})
-          local jid = start("run", "--socket", server)
-          if jid == -1 then
-            vim.api.nvim_err_writeln("Error! - Invalid job!")
-          elseif jid == 0 then
-            vim.api.nvim_err_writeln("Error! - Not executable!")
-          else
-            job_id = jid
-          end
+          job_id = start("run", "--socket", server)
         end
 
-        if not job_id then
-          return
-        elseif not err_exit and _G[cmd] then
+        if not err_exit and _G[cmd] then
           _G[cmd](args)
         else
           defer(
--- plugin/chadtree.vim
+++ plugin/chadtree.vim
@@ -1,6 +1,16 @@
 autocmd VimEnter * silent! autocmd! FileExplorer
 silent! autocmd! FileExplorer
 
+function CHADon_exit(_, code, __)
+  call luaeval('chad.on_exit(...)', [a:code])
+endfunction
+function CHADon_stdout(_, msg, __)
+  call luaeval('chad.on_stdout(...)', [a:msg])
+endfunction
+function CHADon_stderr(_, msg, __)
+  call luaeval('chad.on_stderr(...)', [a:msg])
+endfunction
 
-let s:top_level = resolve(expand('<sfile>:p:h:h'))
-call luaeval('require("chadtree")(...)', [s:top_level])
+
+let s:chad_top = resolve(expand('<sfile>:p:h:h'))
+call luaeval('require("chadtree")(...)', [s:chad_top])
