1f65290ed0b05a366a9af73bc2c245bf1147b318
Fly me to the moon <git@bigly.dog>
2021-01-28 19:36:34 -0800

    gme stronks

--- venv.bat
+++ venv.bat
@@ -1,5 +1,15 @@
 @echo off
 cd /D "%~dp0"
-set path=%cd%\.vars\runtime\Scripts;%PATH%
-%*
+set "path=%cd%\.vars\runtime\Scripts;%PATH%"
+
+i exist "%1" (
+  if exist "%cd%\.vars\runtime\Scripts\python.exe" (
+    shift
+    python %*
+  ) else (
+    %*
+  )
+) else (
+  %*
+)
 @echo on
