76cf7c8e7cd92a43f2c9b0a128f409423c31ee49
I love my dog <git@bigly.dog>
2021-06-21 15:14:23 -0700

    better notify

--- chadtree/lsp/notify.lua
+++ chadtree/lsp/notify.lua
@@ -1,4 +1,5 @@
-(function (method, params)
+(function (args)
+  local method, params = unpack(args)
   if vim.lsp then
     local clients = vim.lsp.get_active_clients()
     for _, client in ipairs(clients) do
