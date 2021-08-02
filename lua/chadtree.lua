9f5ecb47274fb6ea68e022a780fd0d88dc6d668a
Fly me to the moon <git@bigly.dog>
2021-08-02 10:11:06 -0700

    wd

--- lua/chadtree.lua
+++ lua/chadtree.lua
@@ -152,7 +152,7 @@ return function(args)
 
     chad.lsp_ensure_capacities = function(cfg)
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities.workspace.workspace.fileOperations = {
+      capabilities.workspace.fileOperations = {
         didCreate = true,
         didRename = true,
         didDelete = true
