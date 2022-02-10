19cc67374fbb98340120f00e0ca61b48b1c58c27
IIIIII <github@bigly.dog>
2022-02-10 11:19:19 -0800

    encode ws properly

--- chadtree/view/ops.py
+++ chadtree/view/ops.py
@@ -1,13 +1,24 @@
 from os import sep
 from os.path import relpath
 from pathlib import Path, PurePath
+from string import whitespace
 
 from ..state.types import State
+
+_WS = {*whitespace} - {"\t"}
+
+
+def encode_for_display(text: str) -> str:
+    encoded = "".join(
+        char.encode("unicode_escape").decode("utf-8") if char in _WS else char
+        for char in text
+    )
+    return encoded
 
 
 def display_path(path: PurePath, state: State) -> str:
     raw = relpath(path, start=state.root.path)
-    name = raw.encode("unicode_escape").decode("utf-8")
+    name = encode_for_display(raw)
     if Path(path).is_dir():
         return f"{name}{sep}"
     else:
