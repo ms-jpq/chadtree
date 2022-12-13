b2821ceb0353b9418f82626f2599ce39eadc3635
root <root@dev.t.home.arpa>
2022-12-13 07:28:08 +0000

    wtf

--- chadtree/version_ctl/git.py
+++ chadtree/version_ctl/git.py
@@ -137,6 +137,7 @@ async def _conv(raw_root: PurePath, raw_stats: _Stats) -> Tuple[PurePath, _Stats
     else:
         proc = await call(cygpath, "--windows", "--", normpath(raw_root))
         cwd = decode(proc.stdout.rstrip())
+        print(raw_root, cwd)
         stdin = encode(
             "\n".join(map(normpath, (raw_root, *(path for _, path in raw_stats))))
         )
