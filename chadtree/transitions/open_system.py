eff27959b3fa0790169b3464653d5d365f048aee
dog <git@bigly.dog>
2023-01-28 15:25:02 -0800

    use startfile under windows

--- chadtree/transitions/open_system.py
+++ chadtree/transitions/open_system.py
@@ -1,12 +1,13 @@
+import sys
 from asyncio import create_task
 from pathlib import PurePath
 from subprocess import CalledProcessError
-from typing import Sequence, Union
 
 from pynvim_pp.logging import suppress_and_log
 from pynvim_pp.nvim import Nvim
 from std2 import anext
 from std2.asyncio.subprocess import call
+from std2.pathlib import AnyPath
 from std2.platform import OS, os
 
 from ..fs.ops import which
@@ -17,22 +18,25 @@ from ..state.types import State
 from .shared.index import indices
 
 
+async def _call(cwd: PurePath, arg0: AnyPath, *args: AnyPath) -> None:
+    try:
+        await call(arg0, *args, cwd=cwd)
+    except CalledProcessError as e:
+        await Nvim.write(e, e.stderr, e.stdout, error=True)
+
+
 async def _open_gui(path: PurePath, cwd: PurePath) -> None:
     with suppress_and_log():
-        if os is OS.macos and (arg0 := which("open")):
-            argv: Sequence[Union[PurePath, str]] = (arg0, "--", path)
+        if sys.platform == "win32":
+            from os import startfile
+
+            startfile(path, cwd=cwd)
+        elif os is OS.macos and (arg0 := which("open")):
+            await _call(cwd, arg0, "--", path)
         elif os is OS.linux and (arg0 := which("xdg-open")):
-            argv = (arg0, path)
-        elif os is OS.windows and (arg0 := which("start")):
-            argv = (arg0, "", path)
+            await _call(cwd, arg0, path)
         else:
             await Nvim.write(LANG("sys_open_err"))
-            return
-
-        try:
-            await call(*argv, cwd=cwd)
-        except CalledProcessError as e:
-            await Nvim.write(e, e.stderr, e.stdout, error=True)
 
 
 @rpc(blocking=False)
